d26d0e584ca5e7d7d399f8101cae028b8076990a
BruhMacaque <70497748+BruhMacaque@users.noreply.github.com>
2022-03-28 20:31:00 -0400

    3

--- app/src/main/java/com/example/jeozone/GameSelectScreen.java
+++ app/src/main/java/com/example/jeozone/GameSelectScreen.java
@@ -8,11 +8,25 @@ import android.view.View;
 import android.widget.Button;
 
 public class GameSelectScreen extends AppCompatActivity {
+    private Button returnToMenu;
+    private int e = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_select_screen);
 
+        returnToMenu = (Button) findViewById(R.id.backButton);
+        returnToMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainMenu();
+            }
+        });
     }
-
+    public void openMainMenu(){
+        Intent intent = new Intent(this, MainActivity.class);
+        e += 1;
+        //startActivity(intent);
+    }
 }
