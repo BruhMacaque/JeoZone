6dc53df32b402177455e9f43c254059e04bc1f57
jatkhat <khatri.jatin17@gmail.com>
2022-04-01 22:37:16 -0400

    Here it is at last!

--- app/src/main/java/com/example/jeozone/GameScreen.java
+++ app/src/main/java/com/example/jeozone/GameScreen.java
@@ -69,6 +69,7 @@ public class GameScreen extends AppCompatActivity {
                 }
                 //Updates the points and congrats message:
                 setContentOfTextView(R.id.points, "" + points);
+
                 setContentOfTextView(R.id.message, message);
                 //PopupWindow continue = new PopupWindow(View view); //If a popup window is being used, then it will be used here.
             }
